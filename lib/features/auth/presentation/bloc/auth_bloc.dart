0b3cf9948dad54f1c54de9b100444096d3229d38
thofelix6 <thofelix6@gmail.com>
2025-10-23 14:53:52 +0800

    ending bloc directory

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,18 +1,51 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
+import 'package:coopdev/features/auth/domain/usecases/login_usecase.dart';
+import 'package:coopdev/features/auth/domain/usecases/sign_up_usecase.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/user_entity.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial());
+  final LoginUseCase loginUseCase;
+  final SignUpUsecase signUpUsecase;
 
-  @override
-  Stream<AuthState> mapEventToState(
-    AuthEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  AuthBloc({
+    required this.loginUseCase,
+    required this.signUpUsecase,
+  }) : super(AuthInitial()){
+    on<LoginEvent>(_onLoginEvent);
+    on<SignUpEvent>(_onSignUpEvent);
+  }
+
+  void _onLoginEvent(LoginEvent event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final result = await loginUseCase(LoginParams(
+        email: event.email,
+        password: event.password)
+    );
+
+    result.fold(
+      (failure)=>emit(AuthError(failure.message)),
+      (user)=>emit(AuthAuthenticated(user)),
+    );
+  }
+
+  void _onSignUpEvent(SignUpEvent event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final result = await signUpUsecase(
+      SignUpParams(
+        email: event.email,
+        password: event.password,
+        name: event.name
+      ),
+    );
+
+    result.fold(
+          (failure)=>emit(AuthError(failure.message)),
+          (user)=>emit(AuthAuthenticated(user)),
+    );
   }
 }
--- lib/features/auth/presentation/bloc/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -1,5 +1,27 @@
 part of 'auth_bloc.dart';
 
-abstract class AuthEvent extends Equatable {
+ abstract class AuthEvent extends Equatable {
   const AuthEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoginEvent extends AuthEvent{
+   final String email;
+   final String password;
+   const LoginEvent({required this.email, required this.password});
+
+   @override
+   List<Object?> get props => [email, password];
+}
+
+class SignUpEvent extends AuthEvent{
+  final String email;
+  final String password;
+  final String name;
+  const SignUpEvent({required this.name, required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password, name];
 }
