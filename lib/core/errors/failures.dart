0b8c01c0ff2a6a7bb8d62ad1193b0c244b87f5af
gusta <gustavemouen@gmail.com>
2025-10-23 10:41:06 +0800

    failures.dart and usecase.dart

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,21 @@
-class Failures{
+import 'package:equatable/equatable.dart';
+abstract class Failures extends Equatable{
+  final String message;
 
+  const Failures(this.message);
+  @override
+  List<Object> get props => [message];
+
+
+}
+class ServerFailure extends Failures{
+  ServerFailure(super.message);
+}
+
+class CacheFailure extends Failures{
+  CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failures{
+  NetworkFailure(super.message);
 }
--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,3 +1,10 @@
-class UseCase{
+import 'package:coopdev/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Params>{
+  Future<Either<Failures, Type>> call (Params params);
+}
+
+class NoParams{
 
 }
