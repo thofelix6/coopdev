9467dc21d8a1fe69289392199b90d24d1f59e24c
gusta <gustavemouen@gmail.com>
2025-10-23 13:02:32 +0800

    user_model.dart finished

---
+++ lib/features/auth/data/model/user_model.dart
@@ -0,0 +1,35 @@
+import 'package:coopdev/features/auth/domain/entities/user_entity.dart';
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable{
+  final String id;
+  final String email;
+  final String name;
+  final String? token;
+
+  UserModel({required this.id, required this.email, required this.name, required this.token});
+
+  factory UserModel.fromJson(Map<String, dynamic> json){
+    return UserModel(id: json['id'] ?? '', email: json['email'] ?? '', name: json['name'] ?? '', token: json['token']);
+  }
+  Map<String, dynamic> toJson(){
+    return {
+      'id':id,
+      'email':email,
+      'name':name,
+      'token':token,
+    };
+  }
+  UserEntity toEntity(){
+    return UserEntity(
+      id:id,
+      email:email,
+      name:name,
+      token:token,
+    );
+  }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id, email, name, token];
+}
