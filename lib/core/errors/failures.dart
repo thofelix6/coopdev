61536d31d9bcddefc72bbec4198d5b711ba1777f
gusta <gustavemouen@gmail.com>
2025-10-23 10:40:19 +0800

    Merge remote-tracking branch 'origin/master'

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,21 +1,3 @@
-import 'package:equatable/equatable.dart';
-abstract class Failures extends Equatable{
-  final String message;
+class Failures{
 
-  const Failures(this.message);
-  @override
-  List<Object> get props => [message];
-
-
-}
-class ServerFailure extends Failures{
-  ServerFailure(super.message);
-}
-
-class CacheFailure extends Failures{
-  CacheFailure(super.message);
-}
-
-class NetworkFailure extends Failures{
-  NetworkFailure(super.message);
 }
--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,10 +1,3 @@
-import 'package:coopdev/core/errors/failures.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class UseCase<Type, Params>{
-  Future<Either<Failures, Type>> call (Params params);
-}
-
-class NoParams{
+class UseCase{
 
 }
