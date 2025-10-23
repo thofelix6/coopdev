264620d5ee1bcd3f4e3fe6ce1097bf8aba9daf6b
gusta <gustavemouen@gmail.com>
2025-10-23 14:42:49 +0800

    auth_state.dart finished

--- lib/features/auth/presentation/bloc/auth_state.dart
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -2,9 +2,26 @@ part of 'auth_bloc.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
-}
-
-class AuthInitial extends AuthState {
   @override
   List<Object> get props => [];
 }
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState{}
+
+class AuthAuthenticated extends AuthState{
+  final UserEntity user;
+
+  const AuthAuthenticated(this.user);
+  @override
+  List<Object> get props => [user];
+}
+class AuthError extends AuthState{
+  final String message;
+
+  const AuthError(this.message);
+
+  @override
+  List<Object> get props => [message];
+ }
